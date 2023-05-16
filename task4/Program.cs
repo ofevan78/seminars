90a4af12b9cc4bfe93745ad321c3633f570f319b
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-16 23:37:36 +0300

    четвертая  задача 1.0

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("ведитете два числа");
+int number = int.Parse(Console.ReadLine());
+int start = int.Parse(Console.ReadLine());
+if (number>start)
+{
+    Console.WriteLine(number + " Максимальое число");
+}
+else
+{
+    Console.WriteLine(start + " Максимальое число");
+}
+
+
+
