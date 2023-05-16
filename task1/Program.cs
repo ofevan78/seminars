40ef81d0ac08664cd6e9857e618b289e063e166e
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-16 20:22:08 +0300

    первоя задача

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,5 +2,25 @@
 int number = int.Parse(Console.ReadLine());
 if (number == 1 )
 {
-    Console.WriteLine()
-}
+    Console.WriteLine( "день недели - понеделник");
+    }
+    else if (number == 2)
+    {
+    Console.WriteLine("день недели -вторник");
+    }
+    else if (number == 3)
+    {
+    Console.WriteLine("день недели - среда");
+    }else if (number == 4)
+    {
+    Console.WriteLine("день недели -четверг");
+    }else if (number == 5)
+    {
+    Console.WriteLine("день недели -пятница");
+    }else if (number == 6)
+    {
+    Console.WriteLine("день недели -суббота");
+    }else if (number == 7)
+    {
+    Console.WriteLine("день недели -воскресение");
+    }
