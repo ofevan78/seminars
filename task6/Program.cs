e372130db40047393763f184a3ffc1bb9ca76a6b
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-19 22:22:31 +0300

    пятая задача  задача 1.4

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("введите число");
+int number = (Console.Read());
+if (number % 2==0)
+{
+    
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("не четное");
+}
