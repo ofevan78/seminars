b8eec2de38e4fa20ae4f21a467c138d3fc2d8897
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-19 21:16:22 +0300

    пятая задача  задача 1.3

---
+++ task5/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите программу которая принимает на вход три числа и выдает максимальное из итих чисел  
+Console.WriteLine("Ведите три числа");
+int numbera = int.Parse(Console.ReadLine());
+int numberb = int.Parse(Console.ReadLine());
+int numberc = int.Parse(Console.ReadLine());
+if (numbera>numberb&&numbera>numberc)
+{
+    Console.WriteLine(numbera + " максимальное число");
+}
+if (numberb>numbera&&numberb>numberc)
+{
+    Console.WriteLine(numberb + " максимальное число");
+}
+if (numberc>numberb&&numberc>numbera)
+{
+    Console.WriteLine(numberc + " максимально число");
+}
+
+
+
+   
