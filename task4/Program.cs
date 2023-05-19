ddd1a0f7c1183edd05b88bb6d35bafa6e781d94c
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-19 19:44:39 +0300

    четвертая  задача 1.1

--- task4/Program.cs
+++ task4/Program.cs
@@ -4,10 +4,13 @@ int start = int.Parse(Console.ReadLine());
 if (number>start)
 {
     Console.WriteLine(number + " Максимальое число");
+    Console.WriteLine(start + " Минимальное число");
+
 }
 else
 {
     Console.WriteLine(start + " Максимальое число");
+    Console.WriteLine(number + " Минимальное число");
 }
 
 
