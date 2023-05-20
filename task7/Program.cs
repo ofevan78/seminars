9866a3971b4af88c4a921c954f6af784fa09ca80
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-20 18:44:56 +0300

    седмая  задача  задача 1.5

---
+++ task7/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("видите число");
+int number = int.Parse(Console.ReadLine());
+
+for (int start = 1; start <= number; start++)
+    if (start % 2 == 0)  
+    {
+
+      Console.WriteLine(  start ); 
+       
+    }
+      
+
