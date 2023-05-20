080cb7796aba4b80dbfe26faeed395b1c0805d67
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-20 20:03:58 +0300

    восьмая  задача  задача 1.6

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,16 +1,40 @@
 ﻿int rand = new Random().Next(10, 100);
 Console.WriteLine(rand);
-int a = rand / 10;
-int b = rand % 10;
-if (a > b)
+int a1 = FindFirstDigitNumber(rand);
+int b1 = FindSecondDigitNumber(rand);
+FindLargestNumber(a1, b1);
+
+int rand1 = new Random().Next(10, 100);
+Console.WriteLine(rand1);
+int a2 = FindFirstDigitNumber(rand1);
+int b2 = FindSecondDigitNumber(rand1);
+FindLargestNumber(a2, b2);
+
+
+void FindLargestNumber(int a, int b)
 {
-    Console.WriteLine("Нйбольшоя цифра числа " + a);
+    if (a > b)
+    {
+        Console.WriteLine("Нйбольшоя цифра числа " + a);
+    }
+    else if (a < b)
+    {
+        Console.WriteLine("Нйбольшоя цифра числа " + b);
+    }
+    else
+    {
+        Console.WriteLine("Числа равны");
+    }
 }
-else if (a<b)
+
+int FindFirstDigitNumber(int r)
 {
-    Console.WriteLine("Нйбольшоя цифра числа " + b);
+    int result = r / 10;
+    return result;
 }
-else
+
+int FindSecondDigitNumber(int r)
 {
-    Console.WriteLine("Числа равны");
+    int result = r % 10;
+    return result;
 }
