984f3227c62d8f072f168048772ecada0225353a
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-20 19:22:42 +0300

    восьмая  задача  задача 1.5

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿int rand = new Random().Next(10, 100);
+Console.WriteLine(rand);
+int a = rand / 10;
+int b = rand % 10;
+if (a > b)
+{
+    Console.WriteLine("Нйбольшоя цифра числа " + a);
+}
+else if (a<b)
+{
+    Console.WriteLine("Нйбольшоя цифра числа " + b);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
