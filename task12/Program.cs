08117925a89bcc16cf02e37dd75848cbe5758f6b
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-20 22:13:38 +0300

    двенадцатая задач добавил метод  1.0

---
+++ task12/Program.cs
@@ -0,0 +1,23 @@
+﻿//Hапишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int rand = new Random().Next(100, 1000);
+Console.WriteLine("рандомное число " + rand);
+int a = FindFirstDigitNumber(rand);
+rand = a % rand;
+int b = Numbertou(rand);
+Console.WriteLine("второе число " + b);
+
+
+int FindFirstDigitNumber(int r)
+{
+    int result = r / 10;
+    return result;
+
+}
+int Numbertou(int r)
+{
+    int result = r%10;
+    return result;
+}
