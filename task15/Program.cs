ca68f776fe650dbdabeef9c77b602e3c2fa9ed4f
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-22 22:52:35 +0300

    15  задач: диопазон четвертьей   1.0

---
+++ task15/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+int qurter=Prompt("ведите Четверть");
+FindQurterPlane(qurter);
+
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int QurterNumber = int.Parse(Console.ReadLine());
+    return QurterNumber;
+}
+void FindQurterPlane(int x )
+{
+    if (x == 1)
+    {
+        Console.WriteLine("Диопазон I четверти по x 0 до +бесконечности по y от 0 до +бесконечности ");
+    }
+    if (x == 2)
+    {
+        Console.WriteLine("Диопазон II четверти по x -бесконечности до 0 по y 0 до +бесконечности  ");
+    }
+    if (x == 3)
+    {
+        Console.WriteLine("Диопазон III четверти по x 0 до -бесконечности по y 0 до -бесконечности");
+    }
+    if (x == 4)
+    {
+        Console.WriteLine("Диопазон IV четверти  по x 0 до +бесконечности по y от 0 до -бесконечности");
+    }
+}
