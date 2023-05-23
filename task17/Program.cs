e7d59b34400690d30ca7beb38d86d91b9425df41
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-23 20:39:26 +0300

    17  задач: таблица квадратов 1.1

---
+++ task17/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+//от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+int n = (Prompt("видите число N"));
+ NumTable(n);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    int numN = Convert.ToInt32(value);
+    return Math.Abs(numN);
+}
+
+void NumTable(int n)
+{
+    int sqr = 1;
+    int currentNum = 1;
+    while (currentNum <= n)
+{
+    sqr = currentNum*currentNum;
+    Console.WriteLine(sqr);
+    currentNum ++;
+}
+}
---
+++ task18/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+int n = (Prompt("видите число N"));
+ NumTable(n);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    int numN = Convert.ToInt32(value);
+    return Math.Abs(numN);
+}
+
+void NumTable(int n)
+{
+    int sqr = 1;
+    int currentNum = 1;
+    while (currentNum <= n)
+{
+    sqr = currentNum*currentNum*currentNum;
+    Console.WriteLine(sqr);
+    currentNum ++;
+}
+}
