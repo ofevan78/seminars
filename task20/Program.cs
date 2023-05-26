3b7ddc84a9765d3ca472357e724ac6b1631ba583
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-26 17:31:00 +0300

    20 задача нахождение палиндрома  1.1

---
+++ task20/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = Prompt("ведиет число");
+int num = n;
+int rev = 0;
+int dig = 0;
+while (num > 0)
+{
+    dig = num % 10;
+    rev = rev * 10 + dig;
+    num = num / 10;
+}
+if (n == rev)
+{
+    Console.WriteLine("да");
+
+}
+if (n < rev )
+{
+    Console.WriteLine("нет");
+}
+if ( n > rev)
+{
+    Console.WriteLine("нет");
+}
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int QurterNumber = int.Parse(Console.ReadLine());
+    return QurterNumber;
+}
