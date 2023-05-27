f0755a4759f63812c01e73c44ba3910a0d36c728
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-27 23:11:33 +0300

    26  задача дз  1.1

--- task26homework/Program.cs
+++ task26homework/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+int NumberSum(int a)
+{
+
+int b = 0;
+if(a<999)
+{ 
+    
+    for (int i = 0; a > i; i++)
+    {
+        b = (a % 10) + (a / 100) + (a / 10 % 10) ;
+    }
+
+    return b;
+}
+if(a>999)
+{ 
+    
+    for (int i = 0; a > i; i++)
+    {
+        b = (a/1000)+(a/100%10)+(a/10%10)+(a%10);
+    }
+
+    return b;
+}
+else  
+{
+   Console.WriteLine("некоректное число");
+   return a;
+
+}
+}
+int Prompt()
+{
+    Console.WriteLine("Ведите число");
+    int product = int.Parse(Console.ReadLine());
+    return product;
+}
+int a = Prompt();
+Console.WriteLine($"{NumberSum(a)}");
