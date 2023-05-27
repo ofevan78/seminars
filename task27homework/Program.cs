ef7276870710ec8c0eb6ad33f877e1ed5d56ea44
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-27 23:16:21 +0300

    27  задача дз  1.1

---
+++ task27homework/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] RandArr(int num)
+{
+int [] array = new int[num];
+for ( int i = 0; i<num; i++)
+{
+    array [i] = new Random().Next(0,999);
+}
+return array;
+}
+void PrintArrya(int[] array)
+{
+    for(int i=0; i < array.Length; i++)
+Console.Write($" [{array[i]}] ");
+Console.WriteLine();
+}
+
+const int lengtharray = 8;
+PrintArrya(RandArr(lengtharray));
