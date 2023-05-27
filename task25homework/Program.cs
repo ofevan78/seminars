12ce1c388a855911309d6f19a552220e924b5c4a
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-27 22:04:54 +0300

    25  задача дз  1.2

--- task25homework/Program.cs
+++ task25homework/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+int NumDegree(int a, int b)
+{
+    int c = a;
+    for(int i = 2; i<=b ;i++)
+    {
+        c = c * a;
+    }
+return c ;
+} 
+int Prompt()
+{
+    Console.WriteLine("Ведите число");
+    int Degree = int.Parse(Console.ReadLine());
+    return Degree;
+}
+int Prompt1()
+{
+    Console.WriteLine("Ведите степень");
+    int Degree = int.Parse(Console.ReadLine());
+    return Degree;
+}
+int a = Prompt();
+int b = Prompt1();
+Console.WriteLine($"число {a} в степни {b} равно {NumDegree(a,b)} ");
