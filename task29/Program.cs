87a9b1da1bf411d68ee51f43d46e9ea955f6d7da
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-30 18:44:48 +0300

    29 задача

---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+int[] GetArray(int size, int maxvalue, int minVaiue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minVaiue, maxvalue + 1);
+        Console.Write($"{res[i]} ");
+
+
+    }
+    return res;
+}
+void NegativeNum(int[] userArray)
+{
+    int arrSize = userArray.Length;
+    for (int i = 0; i < arrSize; i++)
+    {
+        userArray[i] *= -1;
+        Console.Write($"{userArray[i]} ");
+
+    }
+
+}
+
+
+
+int[] arr = GetArray(4, 5, -5);
+Console.WriteLine();
+NegativeNum(arr);
