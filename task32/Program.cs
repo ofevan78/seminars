19936f168b6a9b7947d03649e19e854b701d49f2
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-30 21:12:28 +0300

    32  задача

---
+++ task32/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6]-> 36 21
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"[{res[i]}] ");
+
+
+    }
+    return res;
+}
+
+void ProductArrayNum(int[] arr)
+{
+    int[] createArr = new int[arr.Length];
+    int maxNum = arr.Length - 1;
+    for (int i = 0; i < (maxNum / 2) + 1; i++)
+    {
+        createArr[i] = arr[i] * arr[maxNum - i];
+        Console.Write($"[{createArr[i]}] ");
+
+    }
+
+}
+int[] array = GetArray(10, -10, 10);
+Console.WriteLine();
+ProductArrayNum(array);
