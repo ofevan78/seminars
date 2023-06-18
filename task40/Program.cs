5dc5c332e27b4eaf5e850bae3a45fdcd99ae50ce
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-06-18 19:25:10 +0300

    40 задача

---
+++ task40/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+
+*/
+
+Console.Write("Введите количество строк массива ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов массива ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns);
+
+
+
+
+int[,] GetArray(int rows, int columns)
+{
+
+    int[,] result = new int[columns, rows];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = i + j;
+            Console.Write($"{result[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
+
+
+
